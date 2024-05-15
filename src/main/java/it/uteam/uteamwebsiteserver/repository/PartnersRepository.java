40f42aec3b5d58d37fc0cb78c4b1e71a409575fc
sayfullobek__ <107360172+sayfullobek@users.noreply.github.com>
2024-05-15 03:43:17 -0700

    salom

--- src/main/java/it/uteam/uteamwebsiteserver/repository/PartnersRepository.java
+++ src/main/java/it/uteam/uteamwebsiteserver/repository/PartnersRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(path = "post", collectionResourceRel = "list", excerptProjection = CustomProjects.class)
+@RepositoryRestResource(path = "partner", collectionResourceRel = "list", excerptProjection = CustomProjects.class)
 @CrossOrigin
 public interface PartnersRepository extends JpaRepository<Partners, Integer> {
 }
--- src/main/java/it/uteam/uteamwebsiteserver/repository/ProjectsRepository.java
+++ src/main/java/it/uteam/uteamwebsiteserver/repository/ProjectsRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(path = "post", collectionResourceRel = "list", excerptProjection = CustomProjects.class)
+@RepositoryRestResource(path = "project", collectionResourceRel = "list", excerptProjection = CustomProjects.class)
 @CrossOrigin
 public interface ProjectsRepository extends JpaRepository<Projects, Integer> {
 }
